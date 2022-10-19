bd1020ce3818fb6ceb575b315e4bb5ad7dab65ee
Veda H Mata <hmata.veda@gmail.com>
2022-10-18 21:35:36 -0500

    Helloworld

---
+++ Fundamentals/Assignment.java
@@ -0,0 +1,7 @@
+
+public class Assignment {
+    public static void main(String[] args) {
+        System.out.println("My name is codingDojo");
+        System.out.println("I am california");
+    }
+}
